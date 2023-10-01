3dbf55dbcab0697589cb6645b49079225f95875e
Salvador Nicolas <snicoper@gmail.com>
2023-10-01 17:50:29 +0200

    Fixed nadbar position

--- src/app/core/config/app-config.ts
+++ src/app/core/config/app-config.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
+import { ThemeColorService } from '../../services/_index';
 
 /**
  * Configuración inicial de la aplicación.
  */
 @Injectable()
 export class AppConfig {
+  constructor(private themeColorService: ThemeColorService) {
+    this.themeColorService.initialize();
+  }
+
   load(): Promise<boolean> {
     return new Promise((resolve) => {
-      // Code here.
       resolve(true);
     });
   }
