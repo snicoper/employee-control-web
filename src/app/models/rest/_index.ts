47dbc5d4c32ffc04ade1ff6fe99f66271dc12f4f
Salvador Nicolas <snicoper@gmail.com>
2023-10-03 22:03:07 +0200

    Add entity base and auditable  interfaces

--- src/app/models/rest/_index.ts
+++ src/app/models/rest/_index.ts
@@ -1,2 +1,4 @@
 export { RefreshTokenModel } from '@models/rest/identity/refresh-token.model';
+export { BaseAuditableModel } from './base-auditable.model';
+export { BaseModel } from './base.model';
 export { RefreshTokenResponse } from './identity/refresh-token.response';
