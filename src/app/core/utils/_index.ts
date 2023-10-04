3f660e856b53f4cd718c4b4bcac9549fa712bd7f
Salvador Nicolas <snicoper@gmail.com>
2023-10-04 23:38:25 +0200

    formatSizeUnit movido como funcion a common-utils

--- src/app/core/utils/_index.ts
+++ src/app/core/utils/_index.ts
@@ -1,6 +1,5 @@
 export { ApiUrls } from './api-urls';
 export { AppEnvironments } from './app-environments';
-export { createGuid, replaceStringParams, toastForNotificationErrors, ucFirst } from './common-utils';
+export { createGuid, formatSizeUnit, replaceStringParams, toastForNotificationErrors, ucFirst } from './common-utils';
 export { debugErrors, raiseError } from './debug-utils';
-export { FormatUtils } from './format-utils';
 export { SiteUrls } from './site-urls';
--- src/app/core/utils/common-utils.ts
+++ src/app/core/utils/common-utils.ts
@@ -54,3 +54,21 @@ export const toastForNotificationErrors = (errors: string[], toastrService: Toas
     toastrService.error(error);
   });
 };
+
+/**
+ * Pasar bytes a una medida legible según el size.
+ *
+ * @param value Bytes a transformar.
+ * @returns Un string con los bytes convertidos a la unidad mas cercana.
+ */
+export const formatSizeUnit = (value: number): string => {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+
+  if (value === 0) {
+    return '0 Byte';
+  }
+
+  const i = parseInt(String(Math.floor(Math.log(value) / Math.log(1024))), 10);
+
+  return `${Math.round(value / Math.pow(1024, i))} ${sizes[i]}`;
+};
