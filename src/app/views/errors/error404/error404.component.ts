7a15d27caab4470802ac830ee6f79291ecc8df51
Salvador Nicolas <snicoper@gmail.com>
2023-10-24 03:02:41 +0200

    Rev views ng-container

--- src/app/views/errors/error404/error404.component.ts
+++ src/app/views/errors/error404/error404.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { SiteUrls } from '@aw/core/urls/_index';
 
 @Component({
   selector: 'aw-error404',
   templateUrl: './error404.component.html'
 })
-export class Error404Component {}
+export class Error404Component {
+  siteUrls = SiteUrls;
+}
