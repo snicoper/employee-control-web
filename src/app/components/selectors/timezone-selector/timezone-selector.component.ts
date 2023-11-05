01313bb530063c2ff47fa3b1d695c7584b25e303
Salvador Nicolas <snicoper@gmail.com>
2023-11-05 03:34:15 +0100

    Rempalzado lib para el selector de timezone

--- src/app/components/selectors/timezone-selector/timezone-selector.component.ts
+++ src/app/components/selectors/timezone-selector/timezone-selector.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { LocalizationService } from '@aw/core/features/localizations/_index';
-import * as moment from 'moment-timezone';
+import { timeZonesNames } from '@vvo/tzdb';
 
 @Component({
   selector: 'aw-timezone-selector',
@@ -9,8 +9,8 @@ import * as moment from 'moment-timezone';
 export class TimezoneSelectorComponent {
   private readonly localizationService = inject(LocalizationService);
 
+  timezones: string[] = timeZonesNames;
   timezoneSelected = this.localizationService.getTimezoneValue();
-  timezones = moment.tz.names();
 
   handleChangeTimezone(): void {
     this.localizationService.setTimezone(this.timezoneSelected);
