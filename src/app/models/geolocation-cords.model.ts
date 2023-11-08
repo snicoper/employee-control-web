664293a6a2c80aecd1bcf93612109d05bc4eb2f5
Salvador Nicolas <snicoper@gmail.com>
2023-11-08 06:52:14 +0100

    Implementado SimpleGeolocationService

---
+++ src/app/models/geolocation-cords.model.ts
@@ -0,0 +1,4 @@
+export interface GeolocationCords {
+  latitude: number;
+  longitude: number;
+}
--- src/app/services/simple-geolocation.service.ts
+++ src/app/services/simple-geolocation.service.ts
@@ -1,14 +1,28 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { urlReplaceParams } from '@aw/core/utils/common-utils';
+import { GeolocationCords } from '@aw/models/_index';
 
 @Injectable({ providedIn: 'root' })
 export class SimpleGeolocationService {
-  private isAvailable = signal(false);
+  private readonly urlOpenStreetMap = 'https://www.openstreetmap.org/#map=18/{latitude}/{longitude}';
 
-  private readonly url = 'https://www.openstreetmap.org/#map=18/{latitude}/{longitude}';
-
-  constructor() {
-    Object.hasOwn(navigator, 'geolocation') ? this.isAvailable.set(true) : this.isAvailable.set(false);
+  get isAvailable(): boolean {
+    return Object.hasOwn(navigator, 'geolocation');
   }
 
-  openStreetMap(): void {}
+  getCurrentPosition(): GeolocationCords {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const coords = { latitude: position.coords.latitude, longitude: position.coords.longitude } as GeolocationCords;
+
+      return coords;
+    });
+
+    return {} as GeolocationCords;
+  }
+
+  getOpenStreetMapLink(latitude: number, longitude: number): string {
+    const url = urlReplaceParams(this.urlOpenStreetMap, { latitude: String(latitude), longitude: String(longitude) });
+
+    return url;
+  }
 }
