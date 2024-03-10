0e53aca0eb61e045ad2107eee6ccacb34dea41fb
Salvador Nicolas <snicoper@gmail.com>
2024-03-10 08:24:16 +0100

    Eliminados tests

--- src/app/views/tests/tests.module.ts
+++ src/app/views/tests/tests.module.ts
@@ -1,14 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { AwCardsModule } from '@aw/components/cards/aw-cards.module';
-import { AwSelectorsModule } from '@aw/components/selectors/aw-selectors.module';
 import { AwViewsModule } from '@aw/components/views/aw-views.module';
-import { AwYearCalendarModule } from '@aw/components/year-calendar/aw-year-calendar.module';
 import { HomeTestComponent } from './home-test/home-test.component';
 import { TestsRoutingModule } from './tests-routing.module';
 
 @NgModule({
   declarations: [HomeTestComponent],
-  imports: [CommonModule, TestsRoutingModule, AwViewsModule, AwCardsModule, AwSelectorsModule, AwYearCalendarModule]
+  imports: [CommonModule, TestsRoutingModule, AwViewsModule]
 })
 export class TestsModule {}
