65d9571d563279403462d7f9d3a9d3422d859bec
Salvador Nicolas <snicoper@gmail.com>
2024-04-05 12:03:15 +0200

    Rename routes files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,74 +6,72 @@ import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./pages/dashboard/dashboard-routing.module').then((m) => m.routes)
+    loadChildren: () => import('./pages/dashboard/dashboard.routes').then((m) => m.routes)
   },
   {
     path: 'accounts',
-    loadChildren: () => import('./pages/accounts/accounts-routing.module').then((m) => m.routes)
+    loadChildren: () => import('./pages/accounts/accounts.routes').then((m) => m.routes)
   },
   {
     path: 'auth',
-    loadChildren: () => import('./pages/auth/auth-routing.module').then((m) => m.routes)
+    loadChildren: () => import('./pages/auth/auth.routes').then((m) => m.routes)
   },
   {
     path: 'category-absences',
     data: { roles: [Roles.HumanResources] },
     canActivate: [AuthGuard],
-    loadChildren: () => import('./pages/category-absences/category-absences-routing.module').then((m) => m.routes)
+    loadChildren: () => import('./pages/category-absences/category-absences.routes').then((m) => m.routes)
   },
   {
     path: 'company-holidays-manage',
     data: { roles: [Roles.HumanResources] },
     canActivate: [AuthGuard],
-    loadChildren: () =>
-      import('./pages/company-holidays-manage/company-holidays-manage-routing.module').then((m) => m.routes)
+    loadChildren: () => import('./pages/company-holidays-manage/company-holidays-manage.routes').then((m) => m.routes)
   },
   {
     path: 'company-settings',
     data: { roles: [Roles.HumanResources] },
     canActivate: [AuthGuard],
-    loadChildren: () => import('./pages/company-settings/company-settings-routing.module').then((m) => m.routes)
+    loadChildren: () => import('./pages/company-settings/company-settings.routes').then((m) => m.routes)
   },
   {
     path: 'tasks',
     data: { roles: [Roles.HumanResources] },
     canActivate: [AuthGuard],
-    loadChildren: () => import('./pages/company-tasks/company-tasks-routing.module').then((m) => m.routes)
+    loadChildren: () => import('./pages/company-tasks/company-tasks.routes').then((m) => m.routes)
   },
   {
     path: 'dashboard',
     data: { roles: [Roles.Employee] },
-    loadChildren: () => import('./pages/dashboard/dashboard-routing.module').then((m) => m.routes)
+    loadChildren: () => import('./pages/dashboard/dashboard.routes').then((m) => m.routes)
   },
   {
     path: 'departments',
     data: { roles: [Roles.HumanResources] },
     canActivate: [AuthGuard],
-    loadChildren: () => import('./pages/departments/departments-routing.module').then((m) => m.routes)
+    loadChildren: () => import('./pages/departments/departments.routes').then((m) => m.routes)
   },
   {
     path: 'employees',
     data: { roles: [Roles.HumanResources] },
     canActivate: [AuthGuard],
-    loadChildren: () => import('./pages/employees/employees-routing.module').then((m) => m.routes)
+    loadChildren: () => import('./pages/employees/employees.routes').then((m) => m.routes)
   },
   {
     path: 'errors',
-    loadChildren: () => import('./pages/errors/errors-routing.module').then((m) => m.routes)
+    loadChildren: () => import('./pages/errors/errors.routes').then((m) => m.routes)
   },
   {
     path: 'time-control-records',
     data: { roles: [Roles.HumanResources] },
     canActivate: [AuthGuard],
-    loadChildren: () => import('./pages/time-control-records/time-control-records-routing.module').then((m) => m.routes)
+    loadChildren: () => import('./pages/time-control-records/time-control-records.routes').then((m) => m.routes)
   },
   {
     path: 'times-control-progress',
     data: { roles: [Roles.Employee] },
     canActivate: [AuthGuard],
-    loadChildren: () =>
-      import('./pages/times-control-progress/times-control-progress-routing.module').then((m) => m.routes)
+    loadChildren: () => import('./pages/times-control-progress/times-control-progress.routes').then((m) => m.routes)
   },
   {
     path: '**',
