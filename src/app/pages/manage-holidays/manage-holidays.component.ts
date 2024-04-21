7abd1fdf00b16bafa73ee952a9d742a3ecc25a73
Salvador Nicolas <snicoper@gmail.com>
2024-04-21 08:29:29 +0200

    Creado component manage-holidays

---
+++ src/app/pages/manage-holidays/manage-holidays.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aw-manage-holidays',
+  templateUrl: './manage-holidays.component.html',
+  styleUrl: './manage-holidays.component.scss',
+  standalone: true,
+  imports: []
+})
+export class ManageHolidaysComponent {}
