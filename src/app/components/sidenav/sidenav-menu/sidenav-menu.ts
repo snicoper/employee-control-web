4e1a601cff59fdab13fdd030006039ab720dc864
Salvador Nicolas <snicoper@gmail.com>
2024-04-26 13:10:11 +0200

    Refactor sidenav

--- src/app/components/sidenav/sidenav-menu/sidenav-menu.ts
+++ src/app/components/sidenav/sidenav-menu/sidenav-menu.ts
@@ -5,5 +5,6 @@ export type SidenavMenu = Array<SidenavMenuItem>;
 export const SidenavMenus: SidenavMenu = [
   { name: 'administration', open: false },
   { name: 'enterprise', open: false },
+  { name: 'calendar', open: false },
   { name: 'employee', open: false }
 ];
