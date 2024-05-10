3e7e14d67c304860110ca54e08c2afcb702be7bd
Salvador Nicolas <snicoper@gmail.com>
2024-05-10 21:45:39 +0200

    ApiResultInterceptor verifica los ResultValue

--- src/app/interceptors/api-result.interceptor.ts
+++ src/app/interceptors/api-result.interceptor.ts
@@ -12,6 +12,8 @@ import { map } from 'rxjs/operators';
 import { ApiResultItemFilter } from '../core/features/api-result/api-result-item-filter';
 import { ApiResultItemOrderBy } from '../core/features/api-result/api-result-item-order-by';
 
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+
 /** Comprueba si es un ApiResult y deserializa filters. */
 @Injectable()
 export class ApiResultInterceptor implements HttpInterceptor {
@@ -19,23 +21,48 @@ export class ApiResultInterceptor implements HttpInterceptor {
     return next.handle(req).pipe(
       map((event) => {
         if (event instanceof HttpResponse && event.status === HttpStatusCode.Ok) {
-          // Order de ApiResult.
-          if (Object.hasOwn(event.body, 'order') && event.body.order.length) {
-            event.body.order = JSON.parse(event.body.order) as ApiResultItemOrderBy;
-          } else if (Object.hasOwn(event.body, 'order')) {
-            event.body.order = '';
-          }
-
-          // Filtros de ApiResult.
-          if (Object.hasOwn(event.body, 'filters') && event.body.filters.length) {
-            event.body.filters = JSON.parse(event.body.filters) as Array<ApiResultItemFilter>;
-          } else if (Object.hasOwn(event.body, 'filters')) {
-            event.body.filters = [] as Array<ApiResultItemFilter>;
-          }
+          event = this.apiResultOrder(event);
+          event = this.apiResultFilter(event);
         }
 
         return event;
       })
     );
+  }
+
+  private apiResultFilter(event: HttpResponse<any>): HttpResponse<any> {
+    // Filtros de ResultValue<ApiResult>.
+    if (Object.hasOwn(event.body?.value, 'filters') && event.body.value.filters.length) {
+      event.body.value.filters = JSON.parse(event.body.value.filters) as Array<ApiResultItemFilter>;
+    } else if (Object.hasOwn(event.body?.value, 'filters')) {
+      event.body.value.filters = [] as Array<ApiResultItemFilter>;
+    }
+
+    // Filtros de ApiResult.
+    if (Object.hasOwn(event.body, 'filters') && event.body.filters.length) {
+      event.body.filters = JSON.parse(event.body.filters) as Array<ApiResultItemFilter>;
+    } else if (Object.hasOwn(event.body, 'filters')) {
+      event.body.filters = [] as Array<ApiResultItemFilter>;
+    }
+
+    return event;
+  }
+
+  private apiResultOrder(event: HttpResponse<any>): HttpResponse<any> {
+    // Order de ResultValue<ApiResult>.
+    if (Object.hasOwn(event.body?.value, 'order') && event.body.value.order.length) {
+      event.body.value.order = JSON.parse(event.body.value.order) as ApiResultItemOrderBy;
+    } else if (Object.hasOwn(event.body?.value, 'order')) {
+      event.body.value.order = '';
+    }
+
+    // Order de ApiResult.
+    if (Object.hasOwn(event.body, 'order') && event.body.order.length) {
+      event.body.order = JSON.parse(event.body.order) as ApiResultItemOrderBy;
+    } else if (Object.hasOwn(event.body, 'order')) {
+      event.body.order = '';
+    }
+
+    return event;
   }
 }
